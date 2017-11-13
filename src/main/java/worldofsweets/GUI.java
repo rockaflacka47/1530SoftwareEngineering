e2d3c332dc65a28265152d603e8133cac5a5961d
David Rocker <der62@pitt.edu>
2017-11-13 16:44:11 -0500

    timer added

--- src/main/java/worldofsweets/GUI.java
+++ src/main/java/worldofsweets/GUI.java
@@ -4,30 +4,61 @@ import java.awt.*;
 import java.awt.event.*; 
 import javax.swing.*;
 import java.util.*;
+//import java.util.Timer;
+import javax.swing.Timer;
 
 public class GUI {
 
 	private final JFrame frame;
 	private ArrayList<JPanel> tileList;
 	private Event event;
-
+	private int ones = 0;
+	private int tens = 0;
+	private int decOnes = 0;
+	private int decTens = 0;
+	JLabel clock = new JLabel();
+	ActionListener updateClockAction = new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			if(++decOnes == 10){
+				decOnes = 0;
+				if(++decTens == 6){
+					decTens = 0;
+					if(++ones == 10){
+						ones = 0;
+						tens++;
+					}
+				}
+			}
+			clock.setText(tens + "" + ones + ":" + decTens + "" + decOnes);
+			frame.pack();
+		}
+	};
+	private Timer timer = new Timer(1000, updateClockAction);
+	
+	public void stopTimer(){
+		timer.stop();
+	}
 	public GUI(Event event) {
 		this.event = event;
-
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("World of Sweets");
 		frame.setResizable(false);
+		timer.start();
 
 		Container pane = frame.getContentPane();
 		pane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		pane.setBackground(GameColor.TABLE);
 		
+		
+        
 		JPanel boardPanel = initializeBoardPanel(null);
 		JPanel dataPanel = initializeDataPanel(null, -1, null);
 
 		pane.add(boardPanel);
 		pane.add(dataPanel);
+		pane.add(clock);
+		
 
 		frame.pack();
 		frame.setVisible(true);
@@ -40,10 +71,11 @@ public class GUI {
 
 		JPanel boardPanel = initializeBoardPanel(playerList);
 		JPanel dataPanel = initializeDataPanel(playerList, turnIndex, card);
-
+		
 		pane.removeAll();
 		pane.add(boardPanel);
 		pane.add(dataPanel);
+		pane.add(clock);
 
 		frame.revalidate();
 		frame.repaint();
